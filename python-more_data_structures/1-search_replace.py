483c5a923a60d571ed75a13a6aaa9b1731ba46e4
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-02 09:44:42 -0400

    push test

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_place(my_list, search, replace):
-    temp
-    for elem in my_list:
-        if (elem == search):
-            temp = elem
-            replace = temp
-return replace
+    return [replace if elem == search else elem for elem in my_list]
