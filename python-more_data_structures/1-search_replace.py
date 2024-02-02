7400c1c66e218ac3c5523563313976789f4b2c79
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-02 09:35:37 -0400

    push test

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_place(my_list, search, replace):
+    temp
+    for elem in my_list:
+        if (elem == search):
+            temp = elem
+        else
+            replace = temp
+
