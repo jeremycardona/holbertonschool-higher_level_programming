0a952724bf366f6462879bbde1dd9aa33977fdce
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 17:02:49 -0400

    push test

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
+    c = 0
     try:
         for i in range(x):
-            print("{}".format(my_list[i]), end='')
-            a += 1
-        except IndexError:
-            pass
-        finally:
-            print()
-        return a
+            print("{}".format(my_list[i]), end="")
+            c += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return c
