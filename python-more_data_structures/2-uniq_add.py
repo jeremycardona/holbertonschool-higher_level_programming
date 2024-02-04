4addc2aea552c0c0ddacf1196ecc6c30fbd91e8f
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 10:39:55 -0400

    push test

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq = set()
-    result = 0
-
-    for n in my_list:
-        if n not in uniq:
-            result += n
-            uniq.add(n)
-
-    return uniq
+    return sum(set(my_list))
