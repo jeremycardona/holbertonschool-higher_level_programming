850a4bce3483b226d0a6163d2ff34cc8a081d4da
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 11:23:15 -0400

    push test

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@ def print_sorted_dictionary(a_dictionary):
     sort_dic = sorted(a_dictionary.keys())
 
     for k in sort_dic:
-        v = a_dictionary[key]
+        v = a_dictionary[k]
         print(f"{k}: {v}")
