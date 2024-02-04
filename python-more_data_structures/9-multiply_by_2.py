f58dc90c473fa4ee4843b92363d4a70eb1097e2c
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 15:44:28 -0400

    push test

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        dict(alt) = a_dictionary.values() * 2
-    return alt
+    dict(alt)
+    return {alt.update(k,v * 2) for k, v in a_dictionary.items()}
