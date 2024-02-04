a75ba5a08993b56026789f8dfe433c798a7432a5
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 17:37:37 -0400

    push test

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_division(a,b):
+def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
         result = None
     finally:
-        print(f"Inside result: {result}")
+        print(F"Inside result: {result}")
         return result
