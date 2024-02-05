fd4c218dbda173f3179fdfd1b91c7fcc6254ac4a
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 22:35:22 -0400

    push test

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Creates a class that defines a square"""
+
+
+class Square:
+    """Class that represents a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
