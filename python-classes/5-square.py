d7aaa6856a4ec168ec5c9a52d1e2d808ca1c4acc
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 23:29:32 -0400

    push test

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""Create a class that defines a square"""
+
+
+class Square:
+    """Class that represents a square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+    """setter method"""
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
