14f31962401d6f90f4e8e5058c3bc4b82e6d1348
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-02-04 22:13:58 -0400

    push test

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Create a class named squared that defines a squares."""
+
+
+class Square:
+    """Class with a private instance field size."""
+    def __init__(self, size):
+    self.__size = size
