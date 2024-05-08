fe36e498dc446b2e6aed9e28b6b22867cd4104eb
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 12:53:27 -0400

    push test

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-numbers = []
 for i in range(10):
     for j in range(i + 1, 10):
-        numbers.append('{:02d}'.format(i * 10 + j))
-
-result = ', '.join(numbers)
-print(result)
+        print('{:02d}'.format(i * 10 + j), end=', ')
