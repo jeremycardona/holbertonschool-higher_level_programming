56c073c64369738f6731e2ad9d95a6efcda5499c
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 13:25:47 -0400

    push test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(s):
     for char in s:
         if 97 <= ord(char) <= 122:
             upper_char = chr(ord(char) - 32)
-            print("{}".format(upper_char), end="")
-        print("")
+        print("{}".format(upper_char), end="")
+    print("")
