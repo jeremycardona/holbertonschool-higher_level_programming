26a2827fee68f7fffd137aa939f663254fbabdbd
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 11:24:21 -0400

    new test

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -12,9 +12,8 @@ last_digit = number % 10
 output_string = f"Last digit of {number} is {last_digit} and "
 
 if last_digit > 5:
-    print(output_string + "greater than 5")
+    print(output_string + "is greater than 5")
 elif last_digit == 0:
     print(output_string + "is 0")
 else:
     print(output_string + "is less than 6 and not 0")
-
---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+i = int(0)
+j = int(100)
+jk = range(i, j)
+while (i < 100):
+#    fi (i <= 9):
+        # print('0')
+    print(i, sep= ',', end="")
+    i += 1
