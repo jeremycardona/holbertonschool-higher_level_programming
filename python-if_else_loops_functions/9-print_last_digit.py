76ec850bf2875681306d851286c75a4a5366950c
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 15:57:17 -0400

    push test

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,6 @@ def print_last_digit(number):
     last_digit = int()
     if (number < 0):
         last_digit = number * -1
-    print(last_digit % 10)
- 
+    last_digit = last_digit % 10
+    print(last_digit)
+    return last_digit
