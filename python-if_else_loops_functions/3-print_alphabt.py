da26ad219102d35da7716264c59231aa72c6d334
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 11:38:10 -0400

    push test

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import str
 
-i = int(0)
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-e = alphabt[5]
-q = alphabt[17]
-print(e)
-# alphabt[i]
-for i in alphabt:
-    if alphabt[int(i)] != e | alphabt[int(i)] != q:
-        print(alphabet)
+for letter in range(97, 123):
+    if chr(letter) == "q" or chr(letter) == "e":
+        continue
+    print("{}".format(chr(letter)), end="")
