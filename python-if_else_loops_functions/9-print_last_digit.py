9c399a789a73992bb3ad5024fce3c798f8c4b6a5
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 15:53:41 -0400

    print last digit

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    last_digit 
+    last_digit = int()
     if (number < 0):
         last_digit = number % -10
     else:
         last_digit = number % 10
     return last_digit
-    
+ 
+p = print_last_digit(96)
+print(p)
