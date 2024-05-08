f094dd38c7eee8ca493270ee9ddc6e99c88f0e1d
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 12:51:57 -0400

    print combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 
-print(', '.join('{:02d}'.format(i * 10 + j) for i in range(10) for j in range(i + 1, 10)))
+numbers = []
+for i in range(10):
+    for j in range(i + 1, 10):
+        numbers.append('{:02d}'.format(i * 10 + j))
+
+result = ', '.join(numbers)
+print(result)
