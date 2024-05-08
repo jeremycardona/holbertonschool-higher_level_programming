2012ca56f9c1ffef2cfeab24119d5d399e6fa553
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 13:20:22 -0400

    push test

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,4 @@
 def uppercase(s):
     for char in s:
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print(uppercase_char, end='')
-    print()
+        print(uppercase_char)
