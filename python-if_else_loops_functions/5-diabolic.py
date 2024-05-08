281feba19a15ecd8263776db2c32840da8657bf8
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 12:29:51 -0400

    push test

--- python-if_else_loops_functions/5-diabolic.py
+++
@@ -1,2 +0,0 @@
-5-print_comb2.py|9 col 17| E251 unexpected spaces around keyword / parameter equals
-5-print_comb2.py|9 col 19| E251 unexpected spaces around keyword / parameter equals
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 
 print(', '.join('{:02d}'.format(i * 10 + j) for i in range(10) for j in range(i + 1, 10)))
 
