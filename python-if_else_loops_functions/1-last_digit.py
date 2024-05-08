73e2ea92586abad9363f1fee11a9e2765a817416
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 11:14:44 -0400

    fix print statements

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,8 +7,8 @@ if number < 0:
 
 print(number)
 if number % 10 > 5:
-    print(f"The last digit of {number} is {number % 10} and greater than 5")
+    print(f"Last digit of {number} is {number % 10} and greater than 5")
 elif number % 10 == 0:
-    print(f"The last digit of {number} is {number % 10} and is 0")
+    print(f"Last digit of {number} is {number % 10} and is 0")
 elif number % 10 < 6:
-    print(f"The last digit {number} is {number % 10} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {number % 10} and is less than 6 and not 0")
