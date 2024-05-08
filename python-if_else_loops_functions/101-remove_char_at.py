be89c850ffcd6ebb9c3a570148606bba486aa494
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-08 16:47:41 -0400

    push test

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    if n < 0 or n >= len(s):
-        return s
+    if n < 0 or n >= len(str):
+        return str
     else:
-        return s[:n] + s[n+1:]
+        return str[:n] + str[n+1:]
