9c57fa2234b7862cd064a78078736fa1f8370416
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-10 16:35:28 -0400

    push test

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-from add_0 import add
+if __name__ == "__main__":
+    
+    from add_0 import add
 
-a = 1
-b = 2
-res = add(a, b)
-
-print(f"{a} + {b} = {res}")
+    a = 1
+    b = 2
+    res = add(a, b)
+    print(f"{a} + {b} = {res}")
