d7a10969f0fd3b126910271457664f384751e67e
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-13 13:05:45 -0400

    push test

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list[:]
-    elif idx > len(my_list) = 1:
+    elif idx > (len(my_list) - 1):
         return my_list[:]
     else:
         temp_list = my_list[:]
