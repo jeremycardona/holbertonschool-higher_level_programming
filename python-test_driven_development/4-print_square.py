a235812db355bf800079485fbb266083fdee55bf
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-21 11:15:12 -0400

    print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""This function prints a square with the character '#'"""
+
+
+def print_square(size):
+    """Method for printing a square with # characters."""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print((("#" * size + "\n") * size), end="")
