8ff5fa214b03d0a014e8ada8991a208d597bfec0
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-21 14:46:16 -0400

    push test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,11 +2,14 @@
 """
     Sum of two integers
 """
+
+
 def add_integer(a, b=98):
     """
         Check that the two variables are integers or floats
         then convert to int and return the sum
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
