47a2cd0fe0fc6b934c7b2ba598a6ba0ad2d13ba7
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-21 08:14:06 -0400

    string representation

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""This creates a class that defines a rectangle"""
+
+
+class Rectangle:
+    """Creates rectangle, calculates the area & perimeter, prints rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width != 0 and self.__height != 0:
+            return 2 * (self.__width + self.__height)
+        else:
+            return 0
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            rectangle_str = ""
+            for i in range(self.__height):
+                rectangle_str += '#' * self.__width + '\n'
+            return rectangle_str[:-1]
