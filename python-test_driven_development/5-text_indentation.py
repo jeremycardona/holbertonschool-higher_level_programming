594ad014ed481a09db9c5244ae8a18c02a6b98cd
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-21 11:22:09 -0400

    text indent

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Print function for a text with 2 new lines"""
+
+
+def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':' """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delimeter in ".?:":
+        text = (delimeter + "\n\n").join(
+            [line.strip(" ") for line in text.split(delimeter)])
+
+    print(text, end="")
