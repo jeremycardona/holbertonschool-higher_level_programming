70b161a1f7eab2ad8c3411be286ae0287335d3f0
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-21 09:14:19 -0400

    say my name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Print 'My name is <first name> <last name>'"""
+
+
+def say_my_name(first_name, last_name="")
+    """Print formatted string with the given first and last name"""
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
