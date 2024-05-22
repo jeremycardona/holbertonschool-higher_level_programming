a8c6cdb7ad4b72a101191b16c4348cc9bd0b0490
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-22 15:01:26 -0400

    push test

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,4 +12,4 @@ def inherits_from(obj, a_class):
     Returns:
     bool: True if obj is an instance of a class that inherited from a_class; otherwise False.
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
