fc2c810d8c668ae5ce21ff2bd74e800ab7fc4a97
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-22 15:03:14 -0400

    push test

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 """Module containing inherits_from function"""
+
+
 def inherits_from(obj, a_class):
     """
-    This function checks if an object is an instance of a class that inherited (directly or indirectly)
+    This function checks if an object
+    is an instance of a class that inherited
+    (directly or indirectly)
     from the specified class.
 
     Parameters:
@@ -10,6 +14,7 @@ def inherits_from(obj, a_class):
     a_class (type): The class to be checked against.
 
     Returns:
-    bool: True if obj is an instance of a class that inherited from a_class; otherwise False.
+    bool: True if obj is an instance of a class that
+    inherited from a_class; otherwise False.
     """
     return type(obj) is not a_class and issubclass(type(obj), a_class)
