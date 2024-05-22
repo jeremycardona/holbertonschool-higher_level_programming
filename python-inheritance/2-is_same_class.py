a80f4592615e4b5144f27665b495d26fb9adeec8
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-22 14:51:20 -0400

    is same class instance

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Function that returns if the object is exacly 
+    an instance of the specified class
+"""
+
+def is_same_class(obj, a_class):
+    """
+        Return true if the object is exacly an instance
+        of the specified class
+    """
+    return type(obj) is a_class
