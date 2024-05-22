72f40a3695f3dc25884344f1ab547ba5278d8889
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-22 14:36:28 -0400

    push test

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Definition for lookup function"""
 
+
 def lookup(obj):
     """
-    This function returns the list of available attributes and methods of an object.
-    
+    This function returns the list of available
+    attributes and methods of an object.
+
     Parameters:
     obj (any): The object whose attributes and methods are to be listed.
 
     Returns:
-    list: A list of strings, each representing an attribute or method of the object.
+    list: A list of strings, each representing
+    an attribute or method of the object.
     """
     if obj:
         return list(dir(obj))
