088aca1ac825a8e79ed277f3e5b60d3786d301d4
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-22 16:02:41 -0400

    push test

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Creates a class"""
+"""Defines a base geometry class BaseGeometry."""
 
 
-class BaseGeometry():
-    """Class with its methods"""
+class BaseGeometry:
+    """Class methods BaseGeometry."""
+
     def area(self):
+        """Raise exception is area is not implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+        """Validate a parameter as an integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value (int): The parameter to validate.
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
