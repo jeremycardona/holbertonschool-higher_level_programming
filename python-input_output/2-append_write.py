bf132c3d7d00065690695b80574cd6405fe8ce22
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-27 10:53:03 -0400

    append to file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for append_write function"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, mode='a', encoding="utf-8") as file:
+        return file.write(text)
