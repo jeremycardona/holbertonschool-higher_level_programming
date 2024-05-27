07f48362f6710c2179ad0c54401f7bbd3ef0bba8
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-27 10:36:04 -0400

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Module for read_file function"""
+
 
 def read_file(filename=""):
+    """Read file"""
     with open(filename, encoding="utf-8") as file:
         fileread = file.read()
         print(fileread)
