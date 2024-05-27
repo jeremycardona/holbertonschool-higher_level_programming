737f88aee000fc5ddef06a7e690f066376207ed7
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-27 10:38:46 -0400

    file read

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Read file"""
     with open(filename, encoding="utf-8") as file:
         fileread = file.read()
-        print(fileread)
+        print(fileread, end="")
