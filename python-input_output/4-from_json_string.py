aa34f6b4bb7ee92dda04f85aab06928c5a30d99e
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-28 12:14:49 -0400

    from json string

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""module for from_json_string"""
-
+"""module for from_json_string function"""
 
 import json
 
 
 def from_json_string(my_str):
-    """return an object represented by a json string"""'
+    """Returns an object represented
+        by a JSON string
+    """
     return json.loads(my_str)
