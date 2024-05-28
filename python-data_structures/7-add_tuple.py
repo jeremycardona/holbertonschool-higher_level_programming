695952395a0966d8959ec453ca659c596c5d5c74
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-28 11:58:24 -0400

    to json string

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,3 +5,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_b += (0, 0)
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return result
+
+p = add_tuple((0,0), (0,0))
+print(p)
---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for to_json_string"""
+
+
+import json
+def to_json_string(my_obj):
+    """return the json representation of a string"""
+    return json.dumps(my_obj)
