b8af5a666f35d1e48dfd6acede786a04e8bb4d4a
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-29 16:17:03 -0400

    pickling custom classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""module that pickles and depickles a custom python object"""
+
+
+import pickle
+
+
+class CustomObject():
+    """custom class fields and methods"""
+    name = str()
+    age = int()
+    is_student = bool()
+
+    def __init__(self, name="John", age=25, is_student=True):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "w") as write_file:
+                pickle.dump(self, write_file)
+        except Exception:
+            print("Error pickling")
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "r") as read_file:
+                return pickle.load(read_file)
+        except Exception:
+            print("Error depickling")
+            return None
