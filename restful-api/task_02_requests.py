023b15df4b6a38f8e2a1f753c116532f9d2f4dcd
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-05-30 14:11:41 -0400

    python requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,40 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    # Print the status code
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+    else:
+        print("Failed to fetch posts")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        data = []
+        for post in posts:
+            data.append({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            })
+        
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            
+            writer.writeheader()
+            for row in data:
+                writer.writerow(row)
+    else:
+        print("Failed to fetch posts")
