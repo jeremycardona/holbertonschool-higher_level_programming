48b9d7d804584128983982ba39bd9686898c2f63
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-17 14:50:25 -0400

    number by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,8 @@
+-- list the number of records with the same score in seconde_table
+-- display the score
+-- display the number of records for this score with the label number
+-- list should be sorted by the number of records
+
+SELECT score, COUNT(score) as number
+FROM second_table
+ORDER BY DESC;
