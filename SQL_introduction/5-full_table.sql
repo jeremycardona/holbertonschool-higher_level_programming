47966cbecdfc52beab201e6115d0b99bc62f0dd1
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-17 13:46:37 -0400

    full table

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,2 @@
 -- Print the schema description of first_table from hbtn_0c_0
-SELECT *
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
