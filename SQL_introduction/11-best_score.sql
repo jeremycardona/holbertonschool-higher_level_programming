1c2528ee3a5752d5727a312a85bbc63af22166a4
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-17 14:18:05 -0400

    best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,6 @@
 -- List all records witha  score >= 10 in second_table
 
 SELECT score, name
-FROM second_table ORDER BY score DESC
-WHERE score >= 10;
+FROM second_table 
+WHERE score >= 10
+ORDER BY score DESC;
