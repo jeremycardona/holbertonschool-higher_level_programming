0d0ca764e3a5343b8e6d2165826eee2f709bfce9
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-19 15:47:20 -0400

    push test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 """query for all states"""
-import sys
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8") 
-    # Create a cursor object
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
-    
-    # Execute the query to select all states sorted by id
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    
-    # Fetch all the results
-    states = cursor.fetchall()
-    
-    # Print the results
-    for state in states:
-        print(state)
-    
-    # Close the cursor and the connection
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
     cursor.close()
     db.close()
