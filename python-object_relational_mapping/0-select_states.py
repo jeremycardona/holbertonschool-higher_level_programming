4a3d6c7a59054571df2ddc31d79a9c268a9df9af
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-19 16:10:23 -0400

    push test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,32 +6,19 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-        passwd=argv[2], db=argv[3], charset="utf8")
-    #!/usr/bin/python3
-"""
-This script lists all states from the database hbtn_0e_0_usa.
-It takes 3 arguments: mysql username, mysql password, and database name.
-"""
-
-from sys import argv
-import MySQLdb
-
-if __name__ == "__main__":
-    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
+        port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3],
-        port=3306
+        charset="utf8"
     )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
 
-    with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        result = cursor.fetchall()
-        if result:
-            print(*result, sep="\n")
-
-    # Close the connection
+    cursor.close()
     db.close()
