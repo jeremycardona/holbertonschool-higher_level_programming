4e75d3c94dc2b77a408194d714483bb8ff33edd3
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-19 15:46:02 -0400

    push test

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,21 +4,20 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8") 
-# Create a cursor object
-cursor = db.cursor()
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], charset="utf8") 
+    # Create a cursor object
+    cursor = db.cursor()
     
-# Execute the query to select all states sorted by id
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    # Execute the query to select all states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     
-# Fetch all the results
-states = cursor.fetchall()
+    # Fetch all the results
+    states = cursor.fetchall()
     
-# Print the results
-for state in states:
-    print(state)
+    # Print the results
+    for state in states:
+        print(state)
     
-# Close the cursor and the connection
-cursor.close()
-db.close()
+    # Close the cursor and the connection
+    cursor.close()
+    db.close()
