bb2de4fbacea15e3ed05d6f3873983a6bd7a3a99
Jeremy Cardona <jeremy.cardona@hotmail.com>
2024-06-20 10:12:18 -0400

    cities by state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""safe query from sql injections"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> \
+              <database name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbname,
+        charset="utf8"
+    )
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Prepare SQL query
+    query = "SELECT * FROM cities ORDER BY id ASC "
+
+    # Execute the query with user input
+    cursor.execute(query)
+
+    # Fetch all the results
+    cities = cursor.fetchall()
+
+    # Print the results
+    for city in cities:
+        print(city)
+
+    # Close the cursor and the connection
+    cursor.close()
+    db.close()
